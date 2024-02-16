57999f25f2b01a0421fec30dec400220c684f514
Williamhsf <williamhenriquefb@gmail.com>
2024-02-16 15:32:45 -0300

    primeiros projetos

---
+++ 07-gerador_paleta_de_cores/scripts.js
@@ -0,0 +1,33 @@
+const generateButton = document.querySelector("#generate-button");
+const colorsDiv = document.querySelector(".colors");
+
+function generateColors() {
+  colorsDiv.innerHTML = "";
+
+  for(let i = 0; i < 5; i++) {
+    const color = generateRandomColor();
+    const colorDiv = document.createElement("div");
+    colorDiv.style.backgroundColor = color;
+    
+    const colorName = document.createElement("p");
+    colorName.innerText = color;
+    colorName.style.color = color;
+    colorDiv.appendChild(colorName);
+    colorsDiv.appendChild(colorDiv);
+
+    console.log(color);
+  }
+}
+
+function generateRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+
+  for(let i = 0; i < 6; i++) {
+    color += letters [Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
+
+generateButton.addEventListener("click", generateColors);
